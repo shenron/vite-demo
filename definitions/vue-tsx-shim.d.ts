48a18286acf5a09b83057551499d8b8b05f8df21
Thomas CASTELLY <thomas@tcy.io>
2021-04-22 11:45:15 +0200

    lint

--- definitions/vue-tsx-shim.d.ts
+++ definitions/vue-tsx-shim.d.ts
@@ -6,6 +6,7 @@ declare module 'vue' {
   // should be removed after Vue supported component events typing
   // see: https://github.com/vuejs/vue-next/issues/1553
   //      https://github.com/vuejs/vue-next/issues/3029
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface ComponentCustomProps extends ComponentsEvents {
   }
 }
