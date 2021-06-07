16019aaa91d49b9e5832892c8ce1b6580b963c25
Thomas CASTELLY <thomas@tcy.io>
2021-06-07 12:05:40 +0200

    add env

---
+++ src/loadEnv.ts
@@ -0,0 +1,5 @@
+export default {
+  VITE_SOME_KEY: import.meta.env.VITE_SOME_KEY,
+  DEV: import.meta.env.DEV,
+  PROD: import.meta.env.PROD,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { createApp } from 'vue';
+import loadEnv from './loadEnv';
 import App from './app/App';
 import router from './router';
 import './index.css';
+
+console.log('print env variables', loadEnv);
 
 createApp(App)
   .use(router)
