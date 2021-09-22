e82f3b2df5ed50e68722e2623ab2c1357fdb57a9
Thomas CASTELLY <thomas@tcy.io>
2021-09-22 14:39:06 +0200

    use arrow function

--- src/components/Foo.tsx
+++ src/components/Foo.tsx
@@ -3,7 +3,5 @@ import { defineComponent } from 'vue';
 // named exports w/ variable declaration: ok
 export const Foo = defineComponent({
   name: 'foo',
-  setup() {
-    return () => <div class="jsx">from JSX</div>;
-  },
+  setup: () => () => <div class="jsx">from JSX</div>,
 });
