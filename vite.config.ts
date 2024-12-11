dd4c1f8f76678e28dde3a584a3abe3fe398ec7a0
Thomas CASTELLY <thomas@castelly.fr>
2024-12-11 15:58:00 +0100

    update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 import path from 'path';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
-  test: {
-    include: ['./tests/unit/**/*.spec.ts'],
-    globals: true,
-    environment: 'jsdom',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
     },
   },
 });
+const vitestConfig = defineVitestConfig({
+  test: {
+    include: ['./tests/unit/**/*.spec.ts'],
+    globals: true,
+    environment: 'jsdom',
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
